29352bf1986bf84378ea884dce812f1b102339a4
JP-sDEV <jon.pablo80@gmail.com>
2025-05-31 09:45:21 -0400

    refactor: include getStaticProps

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -7,11 +7,46 @@ import settings from '../../content/_settings.json'
 import colors from '../../content/projects/_colors.json'
 
 //
-export default function Projects({ user, repos }) {
+
+export async function getStaticProps() {
+  try {
+    const res = await fetch(`${process.env.CMS_API_URL}/api/projects?populate[medias][populate]=*`);
+
+    if (!res.ok) {
+      return {
+        props: {
+          projects: null,
+          error: true
+        }
+      };
+    }
+
+    const data = await res.json();
+    return {
+      props: {
+        projects: data,
+        error: false
+      },
+      // revalidate: 60,
+    };
+
+  } catch (err) {
+    console.error("Failed to fetch projects: ", err);
+    return {
+      props: {
+        projects: null,
+        error: true
+      }
+    };
+  }
+}
+
+
+export default function Projects({ projects, error }) {
 	return (
 		<>
 		<Color colors={colors} />
-		<FeaturedProjects />
+		<FeaturedProjects projects={projects} error={error}/>
 		</>
 	)
 }

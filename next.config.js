9fbd6b0e0ac68874bc9650e84f29017cea692469
JP-sDEV <jon.pablo80@gmail.com>
2025-05-31 09:53:39 -0400

    feat: add strapi

--- next.config.js
+++ next.config.js
@@ -1,40 +1,59 @@
 module.exports = {
-	reactStrictMode: true,
-	env: {
-		dir: '/',
-	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: '**.vercel.app',
-				pathname: '/api/**'
-			},
-			{
-				protocol: 'https',
-				hostname: '**.vercel.app',
-				pathname: '?app=portfolio-theme-jqe0jhmif-atlamors.vercel.app'
-			},
-			{
-				protocol: 'https',
-				hostname: '**.shields.io',
-				pathname: '/badge/**'
-			},
-			{
-				protocol: 'https',
-				hostname: '**.shields.io',
-				pathname: '/github/**'
-			},
-			{
-				protocol: 'https',
-				hostname: '**.githubusercontent.com',
-				pathname: '/**'
-			},
-			{
-				protocol: 'https',
-				hostname: '**.medium.com',
-				pathname: '/**'
-			},
-		],
-	},
-}
+  reactStrictMode: false,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**.vercel.app",
+        pathname: "/api/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**.vercel.app",
+        pathname: "?app=portfolio-theme-jqe0jhmif-atlamors.vercel.app",
+      },
+      {
+        protocol: "https",
+        hostname: "**.shields.io",
+        pathname: "/badge/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**.shields.io",
+        pathname: "/github/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**.githubusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**.medium.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**.youtube.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**.strapiapp.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
